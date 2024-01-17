f38eab66c3cb70ed978eb9febaa3a40895590822
Emma Mellgren <mellgrenemma0@gmail.com>
2024-01-17 20:09:42 +0100

    Updated styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,20 @@
-const ProductCard = ({product}) => {
+import { Link } from 'react-router-dom';
 
-  return (
-    <div className="rounded-lg bg-white overflow-hidden border border-slate-300 shadow-md shadow-slate-400">
-      <div className="border-b border-slate-300">
-        <h3 className="px-6 pt-6 text-sm text-gray-700">{product.name}</h3>
-        <p className="px-6 pt-2 pb-6 text-lg font-semibold text-gray-900">{product.price}</p>
-      </div>
-      <div className="mb-12">
-        <img src={product.images[0]} alt={product.name} className="w-full h-full object-contain"/>
-      </div>
-    </div>
-  )
-}
-export default ProductCard
+const ProductCard = ({ product }) => {
+    return (
+        <div className='rounded-lg bg-white overflow-hidden border border-slate-300 shadow-md shadow-slate-400'>
+            <div className='border-b border-slate-300 px-6 py-6'>
+                <Link to='/product/:id'>
+                    <h3 className='text-sm text-gray-700 hover:text-gray-900 hover:underline'>{product.name}</h3>
+                </Link>
+                <p className='pt-2 text-lg font-semibold text-gray-900'>{product.price}</p>
+            </div>
+            <div className='mb-12'>
+                <Link to='/product/:id'>
+                    <img src={product.images[0]} alt={product.name} className='w-full h-full object-contain transition hover:opacity-85' />
+                </Link>
+            </div>
+        </div>
+    );
+};
+export default ProductCard;
