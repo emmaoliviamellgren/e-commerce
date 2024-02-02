19de2b619c9047fef022c2072817d236fb1bfcb9
Emma Mellgren <mellgrenemma0@gmail.com>
2024-02-02 13:04:54 +0100

    Update ShoppingCartButton component to display cart items and allow adding/removing items

--- src/components/ShoppingCartButton.jsx
+++ src/components/ShoppingCartButton.jsx
@@ -3,12 +3,11 @@ import { LuShoppingCart } from 'react-icons/lu';
 import { FaShoppingCart } from 'react-icons/fa';
 import { CartContext } from '../contexts/CartContext';
 import { Fragment, useContext } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const ShoppingCartButton = () => {
-
-    const { cartItems, addToCart, displayAmountOfItems } =
-    useContext(CartContext);
+    const { cartItems, addToCart, removeFromCart, displayAmountOfItems } =
+        useContext(CartContext);
 
     return (
         <Popover className='relative'>
@@ -34,16 +33,88 @@ const ShoppingCartButton = () => {
                         leave='transition ease-in duration-150'
                         leaveFrom='opacity-100 translate-y-0'
                         leaveTo='opacity-0 translate-y-1'>
-                        <Popover.Panel className='absolute left-1/2 z-10 mt-3 w-[180px] max-w-sm lg:max-w-2xl -translate-x-24 md:-translate-x-1/2 transform px-4 sm:px-0'>
+                        <Popover.Panel className='absolute z-10 mt-3 w-96 max-w-sm lg:max-w-2xl -translate-x-24 md:-translate-x-1/2 transform px-4 sm:px-0'>
                             <div className='rounded-lg bg-white overflow-hidden border border-slate-300 shadow-md shadow-slate-400'>
-                                <div className='bg-gray-50 p-4 text-center'>
+                                <div className='bg-gray-50 p-4 flex flex-col'>
+                                    {/* If there are items in cart, display them */}
                                     {cartItems.length > 0 ? (
-                                        <NavLink
-                                            to='/checkout'
-                                            className='flex gap-1 justify-center items-center text-sm bg-orange-700 text-white transition hover:opacity-75 px-4 py-2 rounded-md shadow-md shadow-orange-400 tracking-wide'>
-                                            <span>Checkout</span>
-                                            <FaShoppingCart />
-                                        </NavLink>
+                                        <>
+                                            <ul role='list' className={`${cartItems.length > 1} && divide-y divide-gray-200`}>
+                                                {cartItems.map((item) => (
+                                                    <li
+                                                        key={item._id}
+                                                        className='flex w-full'>
+                                                        <div className='h-16 w-16 overflow-hidden rounded-md border border-gray-200 shadow-md mt-3'>
+                                                            <img
+                                                                src={
+                                                                    item
+                                                                        .images[0]
+                                                                }
+                                                                alt={item.name}
+                                                                className='h-16 w-16 object-contain'
+                                                            />
+                                                        </div>
+
+                                                        <div className='flex flex-col ml-4 my-3'>
+                                                            <div className='text-xs font-medium text-gray-900'>
+                                                                <p>
+                                                                    <Link
+                                                                        to={`/product/${item._id}`}
+                                                                        className='hover:underline'>
+                                                                        {
+                                                                            item.name
+                                                                        }
+                                                                    </Link>
+                                                                </p>
+                                                                <p className=' font-semibold text-sm mt-2'>
+                                                                    {item.price} SEK
+                                                                </p>
+                                                            </div>
+                                                            <div className='flex text-xs items-center justify-between w-full'>
+                                                                {/* Item Quantity */}
+                                                                <div className='flex items-center justify-between my-3 gap-4'>
+                                                                    <p
+                                                                        className='text-gray-500 p-2 bg-slate-200 rounded-lg font-semibold w-13
+                                                                '>
+                                                                        Qty:{' '}
+                                                                        {
+                                                                            item.quantity
+                                                                        }
+                                                                    </p>
+                                                                    <div className='flex gap-1'>
+                                                                        {/* Buttons */}
+                                                                        <button
+                                                                            className='px-3 py-1 rounded-md border border-gray-200 shadow-md'
+                                                                            onClick={() => {
+                                                                                removeFromCart(
+                                                                                    item
+                                                                                );
+                                                                            }}>
+                                                                            -
+                                                                        </button>
+                                                                        <button
+                                                                            className='px-3 py-1 rounded-md border border-gray-200 shadow-md'
+                                                                            onClick={() => {
+                                                                                addToCart(
+                                                                                    item
+                                                                                );
+                                                                            }}>
+                                                                            +
+                                                                        </button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                            <NavLink
+                                                to='/checkout'
+                                                className='flex gap-1 justify-center items-center text-sm bg-orange-700 text-white transition hover:opacity-75 px-4 py-2 rounded-md shadow-md shadow-orange-400 tracking-wide'>
+                                                <span>Checkout</span>
+                                                <FaShoppingCart />
+                                            </NavLink>
+                                        </>
                                     ) : (
                                         <p className='text-sm text-gray-300'>
                                             Your cart is empty
