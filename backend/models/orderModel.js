ee4b426d40d0e68c5bfe7ef1f579f7d3143bdcb5
Emma Mellgren <mellgrenemma0@gmail.com>
2024-03-16 17:53:52 +0100

    Update user token in postOrder function

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -4,13 +4,13 @@ const User = require('../schemas/userSchema');
 exports.postOrder = async (req, res) => {
     try {
         // Check if user is logged in
-        const user = await User.findOne({ email: req.user.email });
+        const user = await User.findOne({ token: req.user.token });
 
         console.log(user);
 
         if (!user) return res.status(401).json({ message: 'Not logged in!' });
 
-        const { productId, totalPrice } = await req.body;
+        const { productId, totalPrice } = req.body;
 
         // Save the order to the database
         const postOrder = await Order.create({
