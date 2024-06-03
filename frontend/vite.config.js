32885138d021fae0ea1efa3bd311b2e032f9a5a8
Emma Mellgren <mellgrenemma0@gmail.com>
2024-06-03 13:46:09 +0200

    updated config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3333',
+        target: 'https://e-commerce-backend-cjxu.onrender.com/',
         changeOrigin: true
       }
     }
