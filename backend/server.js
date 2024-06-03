cab7e43c68023ca91f992bc982030902f8cfe6de
Emma Mellgren <mellgrenemma0@gmail.com>
2024-06-03 17:19:54 +0200

    added server variable

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const app = require('./app')
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3333
+// const PORT = process.env.PORT || 3333
 const MONGOURI = process.env.MONGO_URI
+const SERVER = process.env.REACT_APP_SERVER
 
 const mongoose = require('mongoose');
 
@@ -10,8 +11,8 @@ mongoose
     .connect(MONGOURI)
     .then(() => {
         console.log('Successfully connected to MongoDB!');
-        app.listen(PORT, () =>
-            console.log('Server is running on: http://localhost:' + PORT)
+        app.listen(SERVER, () =>
+            console.log('Server is running on: ' + SERVER)
         );
     })
     .catch((error) => console.log('Could not connect to MongoDB: ' + error));
