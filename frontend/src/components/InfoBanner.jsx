3973667f268fe67339936fc14fbb5b9d206b77a6
Emma Mellgren <mellgrenemma0@gmail.com>
2024-06-06 14:33:21 +0200

    placement fix

--- frontend/src/components/InfoBanner.jsx
+++ frontend/src/components/InfoBanner.jsx
@@ -1,15 +1,17 @@
 import { XMarkIcon } from '@heroicons/react/20/solid';
 import { useState } from 'react';
 
-const [isOpen, setIsOpen] = useState(false);
-
-if (!isOpen) return null;
 
 export const InfoBanner = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    if (!isOpen) return null;
+
     return (
-        <div className='relative isolate flex items-center gap-x-6 overflow-hidden bg-slate-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1'>
+        <div className='relative isolate flex items-center gap-x-6 overflow-hidden bg-orange-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1'>
             <div className='flex flex-wrap items-center gap-x-4 gap-y-2'>
-                <p className='text-xs font-medium leading-6 text-gray-900'>
+                <p className='text-xs font-medium leading-6 text-white'>
                     Vid första rendering av sidan kan servern ta upp till 30 sek
                     att svara på grund av begränsningar hos Render. Jag jobbar
                     på att förbättra prestandan.
@@ -22,7 +24,7 @@ export const InfoBanner = () => {
                     onClick={() => setIsOpen(false)}>
                     <span className='sr-only'>Dismiss</span>
                     <XMarkIcon
-                        className='h-5 w-5 text-gray-900'
+                        className='h-5 w-5 text-white'
                         aria-hidden='true'
                     />
                 </button>
