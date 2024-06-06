a16d4b860675fbd0d6a5410375cfc0e84d88c9e7
Emma Mellgren <mellgrenemma0@gmail.com>
2024-06-06 14:21:14 +0200

    Added banner

--- frontend/src/app/public/HomePage.jsx
+++ frontend/src/app/public/HomePage.jsx
@@ -1,11 +1,14 @@
 // Components
 import Hero from '../../components/Hero';
+import { InfoBanner } from '../../components/InfoBanner';
 import ProductGrid from '../../components/ProductGrid';
 import FilterByCategory from '../../features/FilterByCategory';
+
 
 function HomePage() {
     return (
         <>
+        <InfoBanner />
             <Hero />
             <div className='text-center my-12'>
                 <h1 className='text-3xl mb-2'>Shop our products</h1>
