d7535b92b141da4a594cabedb9619aa555dc4ae2
Emma Mellgren <mellgrenemma0@gmail.com>
2025-03-09 21:20:16 +0100

    Removed info banner

--- frontend/src/app/public/HomePage.jsx
+++ frontend/src/app/public/HomePage.jsx
@@ -1,22 +1,19 @@
 // Components
 import Hero from '../../components/Hero';
-import { InfoBanner } from '../../components/InfoBanner';
 import ProductGrid from '../../components/ProductGrid';
 import FilterByCategory from '../../features/FilterByCategory';
 
-
 function HomePage() {
-    return (
-        <>
-        <InfoBanner />
-            <Hero />
-            <div className='text-center my-12'>
-                <h1 className='text-3xl mb-2'>Shop our products</h1>
-                <p>Browse the collection...</p>
-            </div>
-            <FilterByCategory />
-            <ProductGrid />
-        </>
-    );
+	return (
+		<>
+			<Hero />
+			<div className='text-center my-12'>
+				<h1 className='text-3xl mb-2'>Shop our products</h1>
+				<p>Browse the collection...</p>
+			</div>
+			<FilterByCategory />
+			<ProductGrid />
+		</>
+	);
 }
 export default HomePage;
